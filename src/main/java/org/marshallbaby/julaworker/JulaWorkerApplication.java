472562213563242418b864f98fa851d0b6360755
Alexey Yatsevich <aleksey.yatsevich@gmail.com>
2024-05-19 14:26:25 +0200

    init skeleton

---
+++ src/main/java/org/marshallbaby/julaworker/JulaWorkerApplication.java
@@ -0,0 +1,13 @@
+package org.marshallbaby.julaworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JulaWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JulaWorkerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/marshallbaby/julaworker/JulaWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.marshallbaby.julaworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JulaWorkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
